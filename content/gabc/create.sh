61500cfdf0c896290fc2bb65441647b02f69e1eb
LeJoW <elle.git.wilke@gmail.com>
2024-07-21 21:16:11 +0200

    First commit

---
+++ content/gabc/create.sh
@@ -0,0 +1,15 @@
+#!zsh
+
+inputTex=$1
+lines=$2
+name=$(basename "$inputTex" .tex)
+outputPdf="build/$name.pdf"
+
+if [ ! -z "$lines" ]; then
+    option="trad-lines-count=$lines"
+else
+    option=
+fi
+
+lualatex --shell-escape --output-directory=build "$inputTex" "$option"
+./pdfcrop --bbox "-1 * * *" "$outputPdf" "$outputPdf"
